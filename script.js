85dcc23868525d4552d3b679d4599af666b74933
joel <joelthousend@gmail.com>
2023-06-23 13:58:13 +0300

    built the structure

---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * store the gameboard as an array inside of a Gameboard object
+ * Your players are also going to be stored in objects
+ * object to control the flow of the game itself.
+ * main goal here is to have as little global code as possible
+ * write a JavaScript function that will render the contents of the gameboard array to the webpage
+ * Build the functions that allow players to add marks to a specific spot on the board
+ * Each little piece of functionality should be able to fit in the game, player or gameboard objects
+ * Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
+ * Clean up the interface to allow players to put in their names
+ * include a button to start/restart the game
+ * add a display element that congratulates the winning player!
+ * Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
+ */
